6577b621898aec5feb15bc54a92d39f91d9dd534
Elijah Townsend <info@established1992.com>
2020-02-05 09:12:15 -0500

    Update GenerateTeams.js

--- GenerateTeams.js
+++ GenerateTeams.js
@@ -1,5 +1,5 @@
 GenerateTeams = {
-	init: function() {
+  init: function() {
     this.cacheDom();
     this.bindEvents();
   },
@@ -7,7 +7,7 @@ GenerateTeams = {
   Cache: {},
   
   cacheDom: function() {
-    this.Cache.$generateteam = $( '#generate-teams' );
+    this.Cache.$addteam = $( '#add-team-button' ); // Add Team Button
     this.Cache.$leaguecontainter = $( '.league-container' );
     this.Cache.$container = $ ( '.container' );
     this.Cache.$teaminfo = $( '.user-container' );
@@ -18,38 +18,21 @@ GenerateTeams = {
   },
   
   bindEvents: function() {
-    this.Cache.$generateteam.on( 'click', function() {
+    this.Cache.$addteam.on( 'click', function() {
       GenerateTeams.quantityPlayers();
-      GenerateTeams.validateContainers(); 
+    });
+
+    this.Cache.$nflteamselect.on( 'change', function() {
     });
 
     this.Cache.$validateteams.on( 'click', function() {
-      GenerateTeams.validateContainers(); 
       GenerateTeams.shuffle( GenerateTeams.Cache.$teamlist );
-    });
-
-    $( document ).on( 'click', '.delete-team', function(e) {
-      const $thisTarget = $( this ).closest( '.user-container' );
-      e.preventDefault();
-      $thisTarget.addClass( 'is-selected' );
-      GenerateTeams.removeTemplate();
-
-      setTimeout(function(){
-        GenerateTeams.validateContainers(); 
-      }, 1000);
+      $( this ).attr( 'disabled', 'true' );
     });
   },
   
   quantityPlayers: function() {
     GenerateTeams.Cache.$leaguecontainter.append( GenerateTeams.containerTemplate() );
-  },
-
-  removeTemplate() {
-    var $thisTarget = $( '.is-selected' );
-
-    setTimeout( function() {
-      $thisTarget.remove();
-    }, 1000 );
   },
 
   containerTemplate: function() {
@@ -65,21 +48,11 @@ GenerateTeams = {
     <label for="team-select-${generatedNumber}">Keeper</label>
 
     <select id="team-select-${generatedNumber}" class="nfl-team__select custom-select" name="nfl-team-dropdown"><option value="none">None</option><option value="Arizona">Arizona</option><option value="Atlanta">Atlanta</option><option value="Baltimore">Baltimore</option><option value="Buffalo">Buffalo</option><option value="Carolina">Carolina</option><option value="Chicago">Chicago</option><option value="Cincinnati">Cincinnati</option><option value="Cleveland">Cleveland</option><option value="Dallas">Dallas</option><option value="Denver">Denver</option><option value="Detroit">Detroit</option><option value="Green Bay">Green Bay</option><option value="Houston">Houston</option><option value="Indianapolis">Indianapolis</option><option value="Jacksonville">Jacksonville</option><option value="Kansas City">Kansas City</option><option value="LA Rams">LA Rams</option><option value="LA Chargers">LA Chargers</option><option value="Miami">Miami</option><option value="Minnesota">Minnesota</option><option value="New England">New England</option><option value="New Orleans">New Orleans</option><option value="NY Jets">NY Jets</option><option value="NY Giants">NY Giants</option><option value="Oakland">Oakland</option><option value="Philadelphia">Philadelphia</option><option value="Pittsburgh">Pittsburgh</option><option value="San Francisco">San Francisco</option><option value="Seattle">Seattle</option><option value="Tampa Bay">Tampa Bay</option><option value="Tennessee">Tennessee</option><option value="Washington">Washington</option></select>
+    <br/>
+    <button class="btn btn-link delete-team">Delete Team</button>
   </div>`;
 
     return template;
-  },
-
-  validateContainers: function() {
-    // Validation for teams so that one is not selected twice
-    var $teamQuantity = $( '.user-container' ).length,
-    $keeperValue = $( 'input' ).find( '.nfl-team__select option:selected' ).val();
-
-    if( $teamQuantity >= 11 ) {
-      $( '#validate-button' ).prop("disabled", true);
-    } else {
-      $( '#validate-button' ).removeAttr("disabled");
-    }
   },
 
   shuffle: function( arra1 ) {
